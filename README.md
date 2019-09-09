c3d5d84c529768051c6f792ff63f88a0b9c51692
Daniel Rodriguez <danducati1098@gmail.com>
2019-09-09 14:04:07 -0400

    My Django Girls app, first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# DR-First-Repository
---
+++ rodv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\19196\djangogirls\rodv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
